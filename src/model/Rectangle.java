d119a4b68ec96643d859172b78ed1fdd37c92c88
dnguyen22 <don.nguyen22@gmail.com>
2017-11-28 08:56:25 -0600

    Add Rectangle extends from Shape

---
+++ src/model/Rectangle.java
@@ -0,0 +1,32 @@
+package model;
+
+import sound.MidiSynth;
+
+import java.awt.*;
+
+public class Rectangle extends Shape {
+    public Rectangle(Point topLeft, MidiSynth midiSynth) {
+        super(topLeft, midiSynth);
+    }
+
+    @Override
+    // EFFECTS: return true if the given Point (x,y) is contained within the bounds of this Shape
+    public boolean contains(Point point) {
+        int point_x = point.x;
+        int point_y = point.y;
+
+        return containsX(point_x) && containsY(point_y);
+    }
+
+    @Override
+    //EFFECTS: draws the shape
+    protected void drawGraphics(Graphics g) {
+        g.drawRect(x, y, width, height);
+    }
+
+    @Override
+    //EFFECTS: fills the shape
+    protected void fillGraphics(Graphics g) {
+        g.fillRect(x, y, width, height);
+    }
+}
